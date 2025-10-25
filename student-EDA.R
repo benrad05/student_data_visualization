ea8e630ed7bb2b2486e0fcbaa3dc7a3d03cf6655
benrad05 <benrad05@gmail.com>
2025-10-25 19:10:21 +0200

    made df

--- student-EDA.R
+++ student-EDA.R
@@ -1,9 +1,24 @@
 # Intro to Data Science - Assignment 1
 # 10/25/2025
+# Daniel Toro and Ben Radoslovich 
 
 rm(list=ls())
 
-x = 5
-y = 10
-cat(x + y)
+library(ggplot2) 
 
+readLines("data/student-mat.csv")
+
+df <- read.csv("data/student-mat.csv",
+               sep = ";",         # semicolon separates fields
+               dec = ".",         # decimal point
+               header = TRUE,     # first row contains column names
+               stringsAsFactors = TRUE)  # categorical columns as factors
+
+# Quick check
+head(df)
+str(df)
+
+
+#3 
+#What feature most strongly correlates with a student's absences? (Ben)
+#   - Heatmap / Bar Chart --> absences vs. numeric vars
